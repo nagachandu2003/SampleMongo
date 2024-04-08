5c4f7c8586f4a8533dbd5cdb5a4825045fb48f02
nagachandu2003 <nagachanduthatha@gmail.com>
2024-04-08 15:11:01 +0530

    changes

--- MongoCRUD/index.js
+++ MongoCRUD/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 const {MongoClient,ObjectId} = require("mongodb")
 const dotenv = require("dotenv")
 // const cors = require("cors")
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 // app.use(cors(
 //     {
 //         origin : 'https://sample-mongo-gi3o.vercel.app/'
