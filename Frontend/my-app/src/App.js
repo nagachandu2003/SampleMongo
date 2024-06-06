000e9a34a0df82ebf2ea2b66e7e332ce81a3f63a
nagachandu2003 <nagachanduthatha@gmail.com>
2024-06-06 12:32:23 +0530

    added news api

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -3,7 +3,7 @@ import {Component} from 'react'
 
 let id
 class App extends Component {
-  state = {dataList:[],username:'',password:'',newpassword:'',username2:'',username3:''}
+  state = {dataList:[],username:'',password:'',newpassword:'',username2:'',username3:'',imgData:''}
 
 
 
@@ -25,6 +25,10 @@ class App extends Component {
   
   onChangeNewPassword = (event) => {
     this.setState({newpassword:event.target.value})
+  }
+
+  onChangeImage = (event) => {
+    this.setState({imgData:event.target.files[0]})
   }
 
   makeUpdate = async () => {
@@ -49,6 +53,18 @@ class App extends Component {
     //   },
     //   body : JSON.stringify()
     // } 
+  }
+
+  getNewsDetails = async () => {
+    try{
+      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`)
+      const data = await response.json()
+      console.log(data);
+    }
+    catch(Err)
+    {
+      console.log(`Error Occurred : ${Err}`);
+    }
   }
 
 
@@ -117,48 +133,94 @@ class App extends Component {
     this.setState({username3:''})
   }
 
+  postImgData = async (imgData) => {
+    try{
+      const options = {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({imgData})
+      }
+      const response = await fetch(`http://localhost:3001/stats`,options)
+      const data = await response.json()
+      console.log(data)
+    }
+    catch(Err){
+      console.log(`Error Occurred : ${Err}`)
+    }
+  }
+
+  onSubmitFile = async (event) => {
+    event.preventDefault();
+    const {imgData} = this.state;
+    const formData = new FormData();
+    formData.append('file', imgData);
+    try {
+        const response = await fetch('http://localhost:3001/upload', {
+          method : "POST",
+            body : formData
+        });
+        const data = await response.json()
+        console.log('File uploaded successfully', data);
+    } catch (error) {
+        console.error('Error uploading file', error);
+    }
+};
 
 
   render(){
-    const {dataList,username,password,newpassword,username2,username3} = this.state
+    const {dataList,username,password,newpassword,username2,username3,imgData} = this.state
+    console.log(imgData)
     return (
       <>
+      <button onClick={this.getNewsDetails}>Get News</button>
       <h1>Hello World</h1>
       <button type="button" onClick={this.getData}>Get Data</button>
       <table>
+        <thead>
         <tr>
           <th>Username</th>
           <th>Password</th>
         </tr>
+        </thead>
+        <tbody>
         {dataList.map((ele) =>  (
           <tr key={ele._id}>
             <td>{ele.username}</td>
             <td>{ele.password}</td>
           </tr>
         ))}
+        </tbody>
       </table>
-      <h1>Post Data</h1>
+      {/* <h1>Post Data</h1>
       <form onSubmit={this.onPostData}>
         <label htmlFor="username">Username</label>
         <input value={username} onChange={this.onChangeUsername} type="text" id="username" placeholder="Enter the Username" />
         <label htmlFor="password">Password</label>
         <input value={password} onChange={this.onChangePassword} type="password" id="password" placeholder="Enter the Password" />
         <button type="submit">Post Data</button>
+      </form> */}
+      <h1>Upload image</h1>
+      <form onSubmit={this.onSubmitFile} encType="multipart/form-data">
+        <label htmlFor="uploadFile">Upload Image</label>
+        <input type="file" id="uploadFile" onChange={this.onChangeImage} name="imagefile"/>
+        <button type='submit'>Submit</button>
       </form>
-      <h1>Update Data</h1>
+      {/* <h1>Update Data</h1>
       <form onSubmit={this.onUpdateData}>
         <label htmlFor="username2">Username</label>
         <input value={username2} onChange={this.onChangeUsername2} type="text" id="username2" placeholder="Enter the Username" />
         <label htmlFor="newpassword">Password</label>
         <input value={newpassword} onChange={this.onChangeNewPassword} type="password" id="newpassword" placeholder="Enter the Password" />
         <button type="submit">Update Data</button>
-      </form>
-      <h1>Delete User</h1>
+      </form> */}
+      {/* <h1>Delete User</h1>
       <form onSubmit={this.onDeleteUser}>
         <label htmlFor="username3">Username</label>
         <input value={username3} id="username3" type="text" placeholder="Enter the username : " onChange={this.onChangeUsername3}/>
         <button type="submit">Delete User</button>
-      </form>
+      </form> */}
       </>
     )
   }
